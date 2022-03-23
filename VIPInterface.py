4727804388e04d8f58deb6af6d1eada347ea6aa5
E_Agboraw <edward.agboraw@gmail.com>
2022-03-23 10:15:27 +0000

    Update VIPInterface.py

--- VIPInterface.py
+++ VIPInterface.py
@@ -1502,3 +1502,39 @@ def saveTest(data):
         with open(strPath+re.sub("h5ad$","img.txt",strH5ad),'w') as f:
             f.write(data['img'])
     return 'success'
+
+def CellPopView(data):
+    
+    adata = createData(data)
+    
+    #subset by cluster
+    adata = adata[adata.obs[data['Cluster]].isin(data['ClusterKey'])]
+    
+    #Split by Condition
+    adata_1 = adata[adata.obs[data['Condition_Key']].isin(data['Cond1'])]
+    adata_2 = adata[adata.obs[data['Condition_Key']].isin(data['Cond2'])]
+    
+    #Extract the data
+    Table_1 = adata_1.to_df()
+    Table_2 = adata_2.to_df()
+    
+    Table_1 = Table_1.transpose() #now genes = rows, cells = columns
+    Expression_1 = Table_1.mean(axis=1) #extracts the average of every row, e.g. average expression of every gene
+
+    Table_2 = Table_2.transpose()
+    Expression_2 = Table_2.mean(axis=1) #average expression of every gene in Cluster 3, Condition S
+    
+    #plot and return graph
+    #CellPopPlot = 
+    plt.scatter(Expression_1,Expression_2, label = "stars", color = "black", 
+                marker = ".",  s =30) 
+    
+    plt.title('Cluster ' + str(Cluster)) 
+    
+    plt.grid()
+    
+    plt.xlabel(Conditions[0])
+    plt.ylabel(Conditions[1], rotation = 0)
+    
+    CellPopPlot = plt.show()
+    return iostreamFig(CellPopPlot)
